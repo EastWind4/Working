02c9dd69dc3d42ef6df50066f793eb3f71eaa406
Yashpb1513 <yashbrahmbhatt26@gmail.com>
2023-02-04 10:42:53 +0530

    Initialized Backend

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const fs = require('fs');
+const dontenv = require('dotenv');
+const cloudinary = require('cloudinary').v2;
+const db = require('./configs/connection');
+
+
+dontenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
+
+const loginRoute = require('./routes/login_route');
+const signupRoute = require('./routes/signup_route');
+const uploadRoute = require('./routes/upload_route');
+// const authRoutes = require('./routes/auth.route');
+// const fileRoutes = require('./routes/file.route');
+// const userRoutes = require('./routes/user.route');
+// const friendRoutes = require('./routes/friend.route');
+
+// Initializing an express app
+const app = express();
+
+// Server Port
+const PORT = process.env.PORT;
+
+// Formatting incoming data and allowing cross origin requests
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Logging incoming requests
+app.use(morgan('dev'));
+
+// API Routes
+app.use('/api/login', loginRoute);
+app.use('/api/signup', signupRoute);
+app.use('/api/image', uploadRoute);
+// app.use('/api/upload', fileRoutes);
+// app.use('/api/user', userRoutes);
+// app.use('/api/friend', friendRoutes);
+
+// Test API
+app.get('/api', (req, res) => {
+    res.status(200).json({
+        name: `${process.env.APP_NAME}`,
+        apiVersion: JSON.parse(fs.readFileSync('./package.json').toString())
+            .version
+    });
+});
+
+// Listening on the port
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
